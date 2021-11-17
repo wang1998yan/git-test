ce501d741185a53a83e31ebb1e5ac438791eea77
Yan <Yan@wangyan.com>
2021-11-17 11:15:15 +0800

    first commit

---
+++ src/main/java/com/nmd/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.nmd.git;
+
+/**
+ * name:JIN
+ * Time:  11:11  2021/11/17
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!!!");
+    }
+}
