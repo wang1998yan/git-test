4e4a881fcc019bf296e0db6cf49b17bf4e955f4e
Yan <Yan@wangyan.com>
2021-11-17 14:35:46 +0800

    second commit

--- src/main/java/com/nmd/git/GitTest.java
+++ src/main/java/com/nmd/git/GitTest.java
@@ -7,5 +7,6 @@ package com.nmd.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!!!");
+        System.out.println("githut!!!");
     }
 }
